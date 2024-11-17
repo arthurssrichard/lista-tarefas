a90889aec4215ebe9effd8be4d4e99772e9f4542
arthurssrichard <arthurssrichard@gmail.com>
2024-11-17 12:46:44 -0300

    Initial Commit

---
+++ resources/views/lista.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<head lang="pt-br">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lista de tarefas</title>
+</head>
+<html>
+    
+</html>
